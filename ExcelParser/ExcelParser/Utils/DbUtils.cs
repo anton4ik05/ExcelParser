eb11d14be6812e97799562ad3b43ff81734b3b8d
a.Novikov <aanto236375@gmail.com>
2022-10-06 11:42:14 +0300

    добавил DbUtils

---
+++ ExcelParser/ExcelParser/Utils/DbUtils.cs
@@ -0,0 +1,23 @@
+﻿using Viten.QueryBuilder;
+using Viten.QueryBuilder.Renderer;
+
+namespace ExcelParser.Utils
+{
+    /// <summary>
+    /// Класс для работы с БД
+    /// </summary>
+    public static class DbUtils
+    {
+        /// <summary>
+        /// Метод получения sql-кода из запроса на выборку
+        /// </summary>
+        /// <param name="select">Запрос на выборку</param>
+        /// <returns>Возвращает sql-код в виде строки</returns>
+        public static string GetSql(Select select)
+        {
+            PostgreSqlRenderer render = new PostgreSqlRenderer();
+
+            return render.RenderSelect(select);
+        }
+    }
+}
